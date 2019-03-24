d8ae01a6d2fc46352f6f9df7cdcc05c501609fea
Student <student@makerlab-5>
2019-03-24 17:40:29 +0200

    fixed bugs

--- fibonacci/fibonacci.cpp
+++ fibonacci/fibonacci.cpp
@@ -7,28 +7,28 @@ using namespace std;
 
 int main(){
 
-	ofstream outfile;
+    ofstream outfile;
     outfile.open("fibonacci.out");
 
-	int n;
-	int f;
-	vector<int> fibonacci;
-	
-	fibonacci.push_back(0);
-	fibonacci.push_back(1);
+    int n;
+    unsigned long long f;
+    vector<unsigned long long> fibonacci;
 
-	cout << "Enter n for fibonacci: " << endl;
-	cin >> n;
+    fibonacci.push_back(0);
+    fibonacci.push_back(1);
 
-	
+    cout << "Enter n for fibonacci: " << endl;
+    cin >> n;
+
+
 //f(n) = f(n-1) + f(n-2)
-	
-	for (int i=0; i<=n; i++){
-		//f=fibonacci.back() + fibonacci.end()[-2];
-		f= fibonacci[i] + fibonacci[i+1];
-		fibonacci.push_back(f);
-		outfile << fibonacci[i] << endl;
-	}
 
-	cout << fibonacci[n] << endl;
+    for (int i=0; i<=n; i++){
+        //f=fibonacci.back() + fibonacci.end()[-2];
+        f= fibonacci[i] + fibonacci[i+1];
+        fibonacci.push_back(f);
+        outfile << fibonacci[i] << endl;
+    }
+
+    cout << fibonacci[n] << endl;
 }
