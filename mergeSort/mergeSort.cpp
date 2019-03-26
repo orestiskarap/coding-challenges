e42e0882e88a70656301899ccae156e0c1e39fb6
Student <student@makerlab-5>
2019-03-26 19:52:15 +0200

    minor fixes

--- mergeSort/mergeSort.cpp
+++ mergeSort/mergeSort.cpp
@@ -1,84 +1,87 @@
-void merge(int arr[], int l, int m, int r) 
-{ 
-    int i, j, k; 
-    int n1 = m - l + 1; 
-    int n2 = r - m; 
+#include <bits/stdc++.h>
+using namespace std;
 
-    int L[n1], R[n2]; 
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
 
-    for (i = 0; i < n1; i++) 
-        L[i] = arr[l + i]; 
-    for (j = 0; j < n2; j++) 
-        R[j] = arr[m + 1+ j]; 
+    int L[n1], R[n2];
 
-    i = 0; 
-    j = 0; 
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    i = 0;
+    j = 0;
     k = l;
-    while (i < n1 && j < n2) 
-    { 
-        if (L[i] <= R[j]) 
-        { 
-            arr[k] = L[i]; 
-            i++; 
-        } 
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
         else
-        { 
-            arr[k] = R[j]; 
-            j++; 
-        } 
-        k++; 
-    } 
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
 
-    while (i < n1) 
-    { 
-        arr[k] = L[i]; 
-        i++; 
-        k++; 
-    } 
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
 
-    while (j < n2) 
-    { 
-        arr[k] = R[j]; 
-        j++; 
-        k++; 
-    } 
-} 
-
-void mergeSort(int arr[], int l, int r) 
-{ 
-    if (l < r) 
-    { 
-
-        int m = l+(r-l)/2; 
-
-        mergeSort(arr, l, m); 
-        mergeSort(arr, m+1, r); 
-
-        merge(arr, l, m, r); 
-    } 
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-void printArray(int A[], int size) 
-{ 
-    int i; 
-    for (i=0; i < size; i++) 
-        printf("%d ", A[i]); 
-    printf("\n"); 
-} 
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
 
-int main() 
-{ 
-    int arr[] = {42, 15, 56, 35, 17, 3}; 
-    int arr_size = sizeof(arr)/sizeof(arr[0]); 
+        int m = l+(r-l)/2;
 
-    printf("Given array is \n"); 
-    printArray(arr, arr_size); 
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
 
-    mergeSort(arr, 0, arr_size - 1); 
+        merge(arr, l, m, r);
+    }
+}
 
-    printf("\nSorted array is \n"); 
+void printArray(int A[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {42, 15, 56, 35, 17, 3};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    printf("\nSorted array is \n");
     printArray(arr, arr_size);
 
 
-    return 0; 
-} 
+    return 0;
+}
